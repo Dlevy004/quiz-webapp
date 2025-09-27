6e27a57a86c160cd6fc7e45b29dd7fae6a7f1ecc
vlajksanyee <vlajksanyi@gmail.com>
2025-09-27 19:09:50 +0200

    feat: add database model and create db

--- quizapp.py
+++ quizapp.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'aa81185a23c8ff7da130ada119bf880a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 @app.route("/")
 @app.route("/auth")
