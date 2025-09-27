2cbef9f7a06a0e1ebc9f297854a0af2a793f92f4
vlajksanyee <vlajksanyi@gmail.com>
2025-09-27 18:57:08 +0200

    feat: add route for profile

--- quizapp.py
+++ quizapp.py
@@ -30,3 +30,8 @@ def register():
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
