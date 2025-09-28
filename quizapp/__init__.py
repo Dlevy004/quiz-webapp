0cc59db195e5e6acf05c9b41a01629188ecf46e6
butyiur <butyiur@gmail.com>
2025-09-28 13:51:08 +0200

    chore(init): import quiz models and run db.create_all()

--- quizapp/__init__.py
+++ quizapp/__init__.py
@@ -3,13 +3,21 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aa81185a23c8ff7da130ada119bf880a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from quizapp import routes
+# --- ÚJ: importáld a kvíz modelleket, hogy a táblák létrejöjjenek
+from quizapp import models_quiz  # noqa: F401
+
+# --- ÚJ: hozd létre a táblákat induláskor
+with app.app_context():
+    db.create_all()
+
+# meglévő route-ok betöltése
+from quizapp import routes  # ez marad
